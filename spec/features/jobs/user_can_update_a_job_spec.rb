5a27ec4417095a3c345dbc947e3ca3c5db9944cc
Megan Arellano <ssciolist@gmail.com>
2018-03-24 13:25:45 -0600

    writes test for update

--- spec/features/jobs/user_can_update_a_job_spec.rb
+++ spec/features/jobs/user_can_update_a_job_spec.rb
@@ -13,12 +13,34 @@ describe 'As a user' do
       expect(current_path).to eq("/companies/#{company.id}/jobs/#{job.id}/edit")
       # expect(page).to have_content('Category')
       expect(page).to have_content('Title')
-      expect(page).to have_content('Company') #Check if this is needed and if yes, grey it out
+      expect(page).to have_content('Company')
       expect(page).to have_content('City')
       expect(page).to have_content('Description')
       expect(page).to have_content('Level of interest')
       expect(page).to have_link('Cancel')
       expect(page).to have_button('Save')
+    end
+
+    it 'I can submit changes to a job' do
+      company_1 = Company.create!(name: "ESPN")
+      company_2 = Company.create!(name: "CNN")
+      job = company_1.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+      visit "/companies/#{company_1.id}/jobs/#{job.id}/edit"
+
+      fill_in('job[title]', with: 'Developer 2.0')
+      select 'CNN', :from => 'job[company_id]'
+      fill_in('job[city]', with: 'Boulder')
+      fill_in('job[description]', with: 'This is a different job')
+      fill_in('job[level_of_interest]', with: 45)
+      click_button('Save')
+
+      expect(current_path).to eq("/companies/#{company_1.id}/jobs/#{job.id}")
+      expect(page).to have_content('Developer 2.0')
+      expect(page).to have_content('CNN')
+      expect(page).to have_content('Boulder')
+      expect(page).to have_content('This is a different job')
+      expect(page).to have_content('45')
     end
   end
 end
