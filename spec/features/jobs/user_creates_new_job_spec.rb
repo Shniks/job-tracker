91b6d17225712ff105debbfe0c2fc07a9a7ed1ae
Megan Arellano <ssciolist@gmail.com>
2018-03-24 18:59:34 -0600

    form changes, but create test failing

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
-describe "User creates a new job" do
-  scenario "a user can create a new job" do
-    company = Company.create!(name: "ESPN")
+describe 'User creates a new job' do
+  scenario 'a user can create a new job' do
+    company = Company.create!(name: 'ESPN')
     category_1 = Category.create!(name: 'Development')
     category_2 = Category.create!(name: 'Production')
 
     visit new_company_job_path(company)
 
-    fill_in "job[title]", with: "Developer"
-    fill_in "job[description]", with: "So fun!"
-    fill_in "job[level_of_interest]", with: 80
-    fill_in "job[city]", with: "Denver"
+    select 'Development', from: 'job[category_id]'
+    fill_in 'job[title]', with: 'Developer'
+    select 'ESPN', from: 'job[company_id]'
+    fill_in 'job[description]', with: 'So fun!'
+    fill_in 'job[level_of_interest]', with: 80
+    fill_in 'job[city]', with: 'Denver'
 
     click_button "Save"
 
     job = company.jobs.last
-
     expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
     expect(page).to have_content("#{job.company.name}")
     expect(page).to have_content("#{job.title}")
