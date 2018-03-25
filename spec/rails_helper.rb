844f379a6a9b6ca447a21d028c75172e8b6aa4fb
Megan Arellano <ssciolist@gmail.com>
2018-03-25 10:11:55 -0600

    hopefully adds DB cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -59,4 +59,16 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  DatabaseCleaner.strategy = :truncation
+
+  RSpec.configure do |c|
+    c.before(:all) do
+      DatabaseCleaner.clean
+    end
+
+    c.after(:each) do
+      DatabaseCleaner.clean
+    end
+    c.include Capybara::DSL
+  end
 end
