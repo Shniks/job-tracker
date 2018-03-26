f2cc849adf971f9eee684a7b434703f9d3f20584
Megan Arellano <ssciolist@gmail.com>
2018-03-26 16:35:54 -0600

    failing form test

--- spec/features/comments/user_can_see_comments_on_job_spec.rb
+++ spec/features/comments/user_can_see_comments_on_job_spec.rb
@@ -14,4 +14,17 @@ describe 'User goes to a job page' do
       expect(page).to have_content(comment.created_at)
     end
   end
+
+  describe 'they see a new comment form' do
+    scenario 'they submit a new comment' do
+      company = Company.create!(name: "ESPN")
+      category = Category.create!(name: "Production")
+      job = Job.create!(title: "Manager", level_of_interest: 80, description: 'Wahoo', city: 'Denver', company: company, category: category)
+
+      visit company_job_path(company, job)
+      fill_in 'comment[content]', with: 'I don\'t want this job'
+
+      expect(page).to have_content('I don\'t want this job')
+    end
+  end
 end
