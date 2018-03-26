5f4f31329941bbd863436751b3e28db621600662
Megan Arellano <ssciolist@gmail.com>
2018-03-26 08:44:55 -0600

    Add failing comment model test, fixes more double quotes in company_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,1 +1,29 @@
 require 'rails_helper'
+
+describe Comment do
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without content' do
+        comment = Comment.new
+        expect(comment).to be_invalid
+      end
+    end
+
+    context 'valid attributes' do
+      it 'is vaild with content' do
+        comment_2 = Comment.new(content: 'The employees seem nice')
+        expect(comment_2).to be_valid
+      end
+    end
+  end
+
+  describe 'relationships' do
+    it 'belongs to a job' do
+      job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+      comment = Comment.new(content: 'Lots of fish here')
+      job.comments << comment
+
+      expect(comment).to belong_to(job)
+    end
+  end
+end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -4,7 +4,7 @@ describe Company do
   describe 'validations' do
     context 'invalid attributes' do
       it 'is invalid without a name' do
-        company = Company.new()
+        company = Company.new
         expect(company).to be_invalid
       end
 
@@ -16,16 +16,16 @@ describe Company do
     end
 
     context 'valid attributes' do
-      it "is valid with a name" do
-        company = Company.new(name: "Dropbox")
+      it 'is valid with a name' do
+        company = Company.new(name: 'Dropbox')
         expect(company).to be_valid
       end
     end
   end
 
-  describe "relationships" do
-    it "has many jobs" do
-      company = Company.new(name: "Dropbox")
+  describe 'relationships' do
+    it 'has many jobs' do
+      company = Company.new(name: 'Dropbox')
       expect(company).to respond_to(:jobs)
     end
   end
