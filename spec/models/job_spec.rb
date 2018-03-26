adec383e628e1472278f3df5a4d74cfd560cbd4f
Megan Arellano <ssciolist@gmail.com>
2018-03-26 08:49:18 -0600

    takes double quotes out of job spec, creates comment model

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,42 +1,42 @@
 require 'rails_helper'
 
 describe Job do
-  describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a title" do
-        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a title' do
+        job = Job.new(level_of_interest: 80, description: 'Wahoo', city: 'Denver')
         expect(job).to be_invalid
       end
 
-      it "is invalid without a level of interest" do
-        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver")
+      it 'is invalid without a level of interest' do
+        job = Job.new(title: 'Developer', description: 'Wahoo', city: 'Denver')
         expect(job).to be_invalid
       end
 
-      it "is invalid without a city" do
-        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80)
+      it 'is invalid without a city' do
+        job = Job.new(title: 'Developer', description: 'Wahoo', level_of_interest: 80)
         expect(job).to be_invalid
       end
     end
 
-    context "valid attributes" do
-      it "is valid with a title, level of interest, company and category" do
-        company = Company.new(name: "Turing")
+    context 'valid attributes' do
+      it 'is valid with a title, level of interest, company and category' do
+        company = Company.new(name: 'Turing')
         category = Category.create!(name: 'Development')
-        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver", company: company, category: category)
+        job = Job.new(title: 'Developer', level_of_interest: 40, city: 'Denver', company: company, category: category)
         expect(job).to be_valid
       end
     end
   end
 
-  describe "relationships" do
-    it "belongs to a company" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+  describe 'relationships' do
+    it 'belongs to a company' do
+      job = Job.new(title: 'Software', level_of_interest: 70, description: 'Wahooo')
       expect(job).to respond_to(:company)
     end
 
-    it "belongs to a category" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+    it 'belongs to a category' do
+      job = Job.new(title: 'Software', level_of_interest: 70, description: 'Wahooo')
       expect(job).to respond_to(:category)
     end
   end
