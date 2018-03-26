21d746984423172f708d4f644a122ae0b5ab4cf4
Megan Arellano <ssciolist@gmail.com>
2018-03-26 16:39:35 -0600

    lints to please the hound

--- spec/features/comments/user_can_see_comments_on_job_spec.rb
+++ spec/features/comments/user_can_see_comments_on_job_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'User goes to a job page' do
   describe 'they see a comment about that job' do
     scenario 'they see the comment and its attributes' do
-      company = Company.create!(name: "ESPN")
-      category = Category.create!(name: "Production")
-      job = Job.create!(title: "Manager", level_of_interest: 80, description: 'Wahoo', city: 'Denver', company: company, category: category)
-      comment = Comment.create!(content: "This seems like a good job", job: job)
+      company = Company.create!(name: 'ESPN')
+      category = Category.create!(name: 'Production')
+      job = Job.create!(title: 'Manager', level_of_interest: 80, description: 'Wahoo', city: 'Denver', company: company, category: category)
+      comment = Comment.create!(content: 'This seems like a good job', job: job)
 
       visit company_job_path(company, job)
 
@@ -17,9 +17,9 @@ describe 'User goes to a job page' do
 
   describe 'they see a new comment form' do
     scenario 'they submit a new comment' do
-      company = Company.create!(name: "ESPN")
-      category = Category.create!(name: "Production")
-      job = Job.create!(title: "Manager", level_of_interest: 80, description: 'Wahoo', city: 'Denver', company: company, category: category)
+      company = Company.create!(name: 'ESPN')
+      category = Category.create!(name: 'Production')
+      job = Job.create!(title: 'Manager', level_of_interest: 80, description: 'Wahoo', city: 'Denver', company: company, category: category)
 
       visit company_job_path(company, job)
       fill_in 'comment[content]', with: 'I don\'t want this job'
