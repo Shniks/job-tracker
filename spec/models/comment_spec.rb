f47a491b57bc196ed846c8d2c21e3837ac37ed6b
Megan Arellano <ssciolist@gmail.com>
2018-03-26 12:31:09 -0600

    All comment model tests pass

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,18 +7,20 @@ describe Comment do
         comment = Comment.new
         expect(comment).to be_invalid
       end
+
+      it 'is invalid without a job' do
+        comment_2 = Comment.new(content: 'The employees seem nice')
+
+        expect(comment_2).to be_invalid
+      end
     end
 
     context 'valid attributes' do
       it 'is vaild with content' do
         job = Job.new(level_of_interest: 80, description: 'Wahoo', city: 'Denver')
-        comment_2 = Comment.new(content: 'The employees seem nice', job: job)
-        expect(comment_2).to be_valid
+        comment_3 = Comment.new(content: 'The employees seem nice', job: job)
+        expect(comment_3).to be_valid
       end
     end
-  end
-
-  describe 'relationships' do
-    it {should belong_to(:job)}
   end
 end
