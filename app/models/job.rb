298b3a91c168dbcd329467766fbf939463733099
Megan Arellano <ssciolist@gmail.com>
2018-03-27 08:23:41 -0600

    cleanup

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,7 @@ class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
   belongs_to :category
-  has_many :comments, , dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def sorted_comments
     comments.order('comments.created_at DESC')
