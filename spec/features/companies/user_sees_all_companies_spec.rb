8c8891b0066d0901aa8a9b868b46e1408e60f29f
Megan Arellano <ssciolist@gmail.com>
2018-03-26 18:02:19 -0600

    Fixing double quotes

--- spec/features/companies/user_sees_all_companies_spec.rb
+++ spec/features/companies/user_sees_all_companies_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe "User sees all companies" do
-  scenario "they see all the companies" do
-    company = Company.create!(name: "ESPN")
-    company_two = Company.create!(name: "Disney")
+describe 'User sees all companies' do
+  scenario 'they see all the companies' do
+    company = Company.create!(name: 'ESPN')
+    company_two = Company.create!(name: 'Disney')
 
     visit companies_path
 
-    expect(page).to have_link("Add a new company")
+    expect(page).to have_link('Add a new company')
     expect(page).to have_content("#{company.name}")
   end
 
