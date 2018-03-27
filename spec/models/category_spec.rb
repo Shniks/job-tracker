ebdda4a97ed29e361919432cd671a7163f0f0193
Megan Arellano <ssciolist@gmail.com>
2018-03-26 20:55:23 -0600

    fixes category error

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
 
-describe Contact do
+describe Category do
   describe 'validations' do
     context 'invalid attributes' do
-      it 'is invalid without content' do
-        comment = Contact.new
-        expect(comment).to be_invalid
+      it 'is invalid without a name' do
+        category = Category.new()
+        expect(category).to be_invalid
       end
 
-      it 'is invalid without a job' do
-        comment_2 = Contact.new(content: 'The employees seem nice')
+      it 'has a unique name' do
+        Category.create!(name: 'Development')
+        category = Category.new(name: 'Development')
 
-        expect(comment_2).to be_invalid
+        expect(category).to be_invalid
       end
     end
 
     context 'valid attributes' do
-      it 'is vaild with content' do
-        job = Job.new(level_of_interest: 80, description: 'Wahoo', city: 'Denver')
-        comment_3 = Contact.new(content: 'The employees seem nice', job: job)
-        expect(comment_3).to be_valid
+      it 'is valid with a name' do
+        category = Category.new(name: 'Development')
+
+        expect(category).to be_valid
       end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      category = Category.new(name: 'Development')
+
+      expect(category).to respond_to(:jobs)
     end
   end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,1 +1,27 @@
+require 'rails_helper'
 
+describe Contact do
+  describe 'validations' do
+    context 'invalid attributes' do
+      skip 'is invalid without attributes' do
+        contact = Contact.new
+        expect(contact).to be_invalid
+      end
+
+      skip 'is invalid without an email' do
+        contact = Contact.new(name: 'Laura', position: 'HR Director')
+        expect(contact).to be_invalid
+      end
+
+      skip 'is invalid without an position' do
+        contact = Contact.new(name: 'Laura', email: 'laura@company')
+        expect(contact).to be_invalid
+      end
+
+      skip 'is invalid without an position' do
+        contact = Contact.new(name: 'Laura', email: 'laura@company')
+        expect(contact).to be_invalid
+      end
+    end
+  end
+end
