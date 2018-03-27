b556f70087bee0522dba46ac78335b70ec300e04
Megan Arellano <ssciolist@gmail.com>
2018-03-26 18:08:24 -0600

    more cleanup

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,9 @@
 class CommentsController < ApplicationController
-
   def create
-
     @job = Job.find(params[:job_id])
     company = Company.find(@job.company_id)
     @comment = Comment.create!(content: params[:comment][:content],
-    job_id: params[:job_id])
+                               job_id: params[:job_id])
     redirect_to company_job_path(company, @job)
   end
 end
