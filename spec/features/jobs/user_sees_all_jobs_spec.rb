a4f096e21a2592cd397b361a803cfba9f11e6d8f
Lihkin <shniks@gmail.com>
2018-03-27 20:01:48 -0600

    Created passing test for jobs sorted by location.

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -44,4 +44,18 @@ describe 'User sees all jobs' do
 
     expect(page.first(:xpath, '//ul')).to have_content("#{job_2.level_of_interest}")
   end
+
+  scenario 'User sees all jobs sorted by location' do
+    company = Company.create!(name: 'ESPN')
+    category = Category.create!(name: 'Development')
+    job_1 = company.jobs.create!(title: 'Developer', level_of_interest: 70, city: 'Denver', category_id: category.id)
+    job_2 = company.jobs.create!(title: 'QA Analyst', level_of_interest: 90, city: 'New York City', category_id: category.id)
+    job_3 = company.jobs.create!(title: 'QA Analyst I', level_of_interest: 65, city: 'Denver', category_id: category.id)
+    job_4 = company.jobs.create!(title: 'QA Analyst II', level_of_interest: 75, city: 'New York City', category_id: category.id)
+    job_5 = company.jobs.create!(title: 'QA Analyst II', level_of_interest: 40, city: 'Austin', category_id: category.id)
+
+    visit '/jobs?sort=location'
+
+    expect(page.first(:xpath, '//ul')).to have_content("#{job_5.city}")
+  end
 end
