5490922f1a3b7db5de53dc0fdb1d11e8175dc1be
Megan Arellano <ssciolist@gmail.com>
2018-03-27 20:05:35 -0600

    Adds failing dashboard test

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   resources :categories
 
+
+  get 'dashboard', to: 'dashboard#show'
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
