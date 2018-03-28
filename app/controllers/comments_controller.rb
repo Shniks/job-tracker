92a8cf843770c5e2032d0d69ed0362637855ef3f
Megan Arellano <ssciolist@gmail.com>
2018-03-28 10:55:53 -0600

    removes div to allow comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    @job = Job.find(params[:job_id])
-    company = Company.find(@job.company_id)
-    @comment = Comment.create!(content: params[:comment][:content],
-                               job_id: params[:job_id])
-    redirect_to job_path(@job)
+    @comment = Comment.new(comment_params)
+    @comment.job_id = params[:job_id]
+    @comment.save
+    redirect_to job_path(params[:job_id])
   end
+end
+
+private
+
+def comment_params
+  params.require(:comment).permit(:content)
 end
